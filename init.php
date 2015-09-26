4a59ddf934f1cfd17e70fefecf3f5a6b2bc223ce
itohsnap <itohsnap@users.noreply.github.com>
2015-09-26 12:12:04 -0700

    Adopt af_readability's preference hooks

--- init.php
+++ init.php
@@ -1,85 +1,121 @@
 <?php
-
 // Initial version of this plugin: https://github.com/atallo/ttrss_fullpost/
-// Now with preference panel, ripped out of: https://github.com/mbirth/ttrss_plugin-af_feedmod
+// Now with preferences ripped out of fox's af_readability plugin: https://tt-rss.org/gitlab/fox/tt-rss/tree/master/plugins/af_readability
 // Relies on the fivefilters port of Readability: http://code.fivefilters.org/php-readability/overview
 
-// Expects the preference field to be URL fragments separated by either newlines:
-//   kotaku.com
-//   destructoid
-//   arstechnica.com
-// or commas:
-//   kotaku.com, destructoid, arstechnica.com
-
-// Note that this will consider the feed to match if the feed's "link" URL contains any
-// element's text. Most notably, Destructoid's posts are linked through Feedburner, and
-// so "destructoid.com" doesn't match--but there is a "Destructoid" in the Feedburner URL,
-// so "destructoid" will. (Link comparisons are case-insensitive.)
-
-class Af_Fullpost extends Plugin implements IHandler
-{
+class Af_Fullpost extends Plugin {
 	private $host;
 
 	function about() {
-		return array(1.30,
-			"Full post (requires CURL).",
-			"atallo");
+		return array(1.40,
+			"Alternate attempt to inline article content using Readability",
+			"itohsnap");
+	}
+	
+	function init($host) {
+		$this->host = $host;
+
+		$host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
+		$host->add_hook($host::HOOK_PREFS_TAB, $this);
+		$host->add_hook($host::HOOK_PREFS_EDIT_FEED, $this);
+		$host->add_hook($host::HOOK_PREFS_SAVE_FEED, $this);
+	}
+	
+	function hook_article_filter($article) {
+		// Stop if we don't have curl (i.e. some way to get the article's HTML)
+		if (!function_exists("curl_init")) return $article;
+		
+		// Stop if the user hasn't checked the Full Post checkbox in this particular feed's settings
+		$enabled_feeds = $this->host->get($this, "enabled_feeds");
+		$key = array_search($article["feed"]["id"], $enabled_feeds);
+		if ($key === FALSE) return $article;
+		
+		// Have Readability parse the page
+		try {
+			$article['content'] = $this->get_full_post($article['link']);
+		} catch (Exception $e) {
+			// Readability failed (?!); don't modify the article's content and keep going
+		}
+		
+		return $article;
+	}
+	
+	function hook_prefs_tab($args) {
+		if ($args != "prefFeeds") return;
+		
+		print "<div dojoType=\"dijit.layout.AccordionPane\" title=\"".__('FullPost settings')."\">";
+		print_notice("Enable the plugin for specific feeds in the feed editor.");
+		
+		$enabled_feeds = $this->host->get($this, "enabled_feeds");
+		if (!array($enabled_feeds)) $enabled_feeds = array();
+		$enabled_feeds = $this->filter_unknown_feeds($enabled_feeds);
+		$this->host->set($this, "enabled_feeds", $enabled_feeds);
+		
+		if (count($enabled_feeds) > 0) {
+			print "<h3>" . __("Currently enabled for (click to edit):") . "</h3>";
+			print "<ul class=\"browseFeedList\" style=\"border-width : 1px\">";
+			foreach ($enabled_feeds as $f) {
+				print "<li>" .
+					"<img src='images/pub_set.png'
+						style='vertical-align : middle'> <a href='#'
+						onclick='editFeed($f)'>".
+					getFeedTitle($f) . "</a></li>";
+			}
+			print "</ul>";
+		}
+		print "</div>";
+	}
+	
+	function hook_prefs_edit_feed($feed_id) {
+		print "<div class=\"dlgSec\">".__("FullPost")."</div>";
+		print "<div class=\"dlgSecCont\">";
+		
+		$enabled_feeds = $this->host->get($this, "enabled_feeds");
+		if (!array($enabled_feeds)) $enabled_feeds = array();
+		$key = array_search($feed_id, $enabled_feeds);
+		$checked = $key !== FALSE ? "checked" : "";
+		
+		print "<hr/><input dojoType=\"dijit.form.CheckBox\" type=\"checkbox\" id=\"af_fullpost_enabled\"
+			name=\"af_fullpost_enabled\"
+			$checked>&nbsp;<label for=\"af_fullpost_enabled\">".__('Fetch full post')."</label>";
+		print "</div>";
+	}
+
+	function hook_prefs_save_feed($feed_id) {
+		$enabled_feeds = $this->host->get($this, "enabled_feeds");
+		if (!is_array($enabled_feeds)) $enabled_feeds = array();
+		$enable = checkbox_to_sql_bool($_POST["af_fullpost_enabled"]) == 'true';
+		$key = array_search($feed_id, $enabled_feeds);
+		
+		if ($enable) {
+			if ($key === FALSE) {
+				array_push($enabled_feeds, $feed_id);
+			}
+		} else {
+			if ($key !== FALSE) {
+				unset($enabled_feeds[$key]);
+			}
+		}
+		$this->host->set($this, "enabled_feeds", $enabled_feeds);
+	}
+	
+	function save() {
+		//
 	}
 	
 	function api_version() {
 		return 2;
-	}
-
-	function init($host) {
-		$this->host = $host;
-
-		$host->add_hook($host::HOOK_PREFS_TABS, $this);
-		$host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
-	}
-	
-	function hook_article_filter($article) {
-		if (!function_exists("curl_init"))
-			return $article;
-		
-		$json_conf = $this->host->get($this, 'json_conf');
-		$owner_uid = $article['owner_uid'];
-		
-		$data = explode(',', str_replace("\n", ",", $json_conf));
-		// trim each element of the array, and then remove empty array elements
-		$data = array_filter(array_map('trim', $data));
-		if (!is_array($data)) {
-			// no valid configuration, or no configuration at all
-			return $article;
-		}
-		
-		foreach ($data as $urlpart) {
-			// skip this entry, if the URL doesn't match
-			if (stripos($article['link'], $urlpart) === false) continue;
-			
-			// do not process an article more than once
-			if (strpos($article['plugin_data'], "fullpost,$owner_uid:") !== false) {
-				if (isset($article['stored']['content'])) $article['content'] = $article['stored']['content'];
-				break;
-			}
-			
-			// have Readability parse the page
-			try {
-				$article['content'] = $this->get_full_post($article['link']);
-				$article['plugin_data'] = "fullpost,$owner_uid:" . $article['plugin_data'];
-			} catch (Exception $e) {
-				// Readability failed (?!); don't modify the article's content and keep going
-			}
-			break;
-		}
-		
-		return $article;
 	}
 	
 	private function get_full_post($request_url) {
 		// now an amalgamation of code from:
 		//   1) https://github.com/feelinglucky/php-readability
 		//   2) http://code.fivefilters.org/php-readability/src
-		include_once 'Readability.php';
+		
+		// Option 1: Use the version of Readability that we've been using (and it has been pretty solid). To use this, you need to include the "Readability.php" and "JSLikeHTMLElement.php" files in the af_fullpost folder.
+		//   include_once 'Readability.php';
+		// Option 2: use TT-RSS' now-built-in copy of Readability, which is ~ the same (but maybe a touch better?)
+		if (!class_exists("Readability")) require_once(dirname(dirname(__DIR__)). "/lib/readability/Readability.php");
 		
 		$handle = curl_init();
 		curl_setopt_array($handle, array(
@@ -95,8 +131,7 @@ class Af_Fullpost extends Plugin implements IHandler
 		$html = curl_exec($handle);
 		curl_close($handle);
 		
-		//if (!$charset = mb_detect_encoding($source)) {
-		//}
+		// Try to fix encoding issues
 		preg_match("/charset=([\w|\-]+);?/", $html, $match);
 		$charset = isset($match[1]) ? $match[1] : 'utf-8';
 		$html = mb_convert_encoding($html, 'UTF-8', $charset);
@@ -110,13 +145,9 @@ class Af_Fullpost extends Plugin implements IHandler
 		}
 		
 		$readability = new Readability($html);
-		// print debug output? 
-		$readability->debug = false;
-		// convert links to footnotes?
-		$readability->convertLinksToFootnotes = false;
-		$result = $readability->init();
+		// $readability->debug = true;
 		
-		if ($result) {
+		if ($readability->init()) {
 			// $title = $readability->getTitle()->textContent;
 			$content = $readability->getContent()->innerHTML;
 			// if we've got Tidy, let's clean it up for output
@@ -133,73 +164,15 @@ class Af_Fullpost extends Plugin implements IHandler
 		return $content;
 	}
 	
-	function hook_prefs_tabs($args)
-	{
-		print '<div id="fullpostConfigTab" dojoType="dijit.layout.ContentPane"
-					href="backend.php?op=af_fullpost"
-					title="' . __('FullPost') . '"></div>';
-	}
-	
-	function index()
-	{
-		$pluginhost = PluginHost::getInstance();
-		$json_conf = $pluginhost->get($this, 'json_conf');
-		
-		print "<p>Comma- or newline-separated list of URLs or URL fragments (e.g. 'destructoid' for the Destructoid Feedburner URL) for sites where you want the full text of each article. Case does not matter.<br>Example: kotaku.com, destructoid, arstechnica.com</p>";
-		print "<form dojoType=\"dijit.form.Form\">";
-		
-		print "<script type=\"dojo/method\" event=\"onSubmit\" args=\"evt\">
-			evt.preventDefault();
-			if (this.validate()) {
-				new Ajax.Request('backend.php', {
-					parameters: dojo.objectToQuery(this.getValues()),
-					onComplete: function(transport) {
-						if (transport.responseText.indexOf('error')>=0) notify_error(transport.responseText);
-						else notify_info(transport.responseText);
-					}
-				});
-				//this.reset();
+	private function filter_unknown_feeds($enabled_feeds) {
+		$tmp = array();
+		foreach ($enabled_feeds as $feed) {
+			$result = db_query("SELECT id FROM ttrss_feeds WHERE id = '$feed' AND owner_uid = " . $_SESSION["uid"]);
+			if (db_num_rows($result) != 0) {
+				array_push($tmp, $feed);
 			}
-			</script>";
-		
-		print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"op\" value=\"pluginhandler\">";
-		print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"method\" value=\"save\">";
-		print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"plugin\" value=\"af_fullpost\">";
-		
-		print "<table width='100%'><tr><td>";
-		print "<textarea dojoType=\"dijit.form.SimpleTextarea\" name=\"json_conf\" style=\"font-size: 12px; width: 99%; height: 500px;\">$json_conf</textarea>";
-		print "</td></tr></table>";
-		
-		print "<p><button dojoType=\"dijit.form.Button\" type=\"submit\">".__("Save")."</button>";
-		
-		print "</form>";
-	}
-	
-	function save()
-	{
-		$json_conf = $_POST['json_conf'];
-		$this->host->set($this, 'json_conf', $json_conf);
-		echo __("Configuration saved.");
-	}
-	
-	function csrf_ignore($method)
-	{
-		$csrf_ignored = array("index", "edit");
-		return array_search($method, $csrf_ignored) !== false;
-	}
-	
-	function before($method)
-	{
-		if ($_SESSION["uid"]) {
-			return true;
 		}
-		return false;
+		return $tmp;
 	}
-	
-	function after()
-	{
-		return true;
-	}
-
 }
 ?>
